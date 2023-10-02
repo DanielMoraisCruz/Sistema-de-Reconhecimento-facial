8fe878d7789b00297bd05590160cf45a43adfdd4
Daniel Cruz <daniel.jack.dmc@gmail.com>
2023-10-02 11:51:06 -0300

    download de fotos
    criei o download das fotos. Fazer uma api não vai rolar aparentemente, vou tentar outras maneiras

--- script/take_photo.js
+++ script/take_photo.js
@@ -8,3 +8,18 @@ navigator.mediaDevices.getUserMedia({video: true})
 .catch(error => {
     console.log(error);
 });
+
+document.querySelector('button').addEventListener('click', () => {
+    var canvas = document.querySelector('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0);
+
+    var link = document.createElement('a');
+    link.download = 'foto.png';
+    link.href = canvas.toDataURL();
+    link.textContent = 'Download Image';
+    link.click();
+    // Preciso fazer com que a foto vá para o banco de dados
+});
