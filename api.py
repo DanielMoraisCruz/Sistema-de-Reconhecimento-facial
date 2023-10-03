9525cbcfb7dad97b2ed9567df3c6de0f24fe6268
Daniel Cruz <daniel.jack.dmc@gmail.com>
2023-10-03 10:43:39 -0300

    Ajustando projeto apenas para API
    Como o front end será feito em Angular, eu só preciso fazer a api

--- api.py
+++ api.py
@@ -1,39 +1,14 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask  # , Response, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('login.html')
+# login |email e senha|
+# cadastro |email, senha, cpf, foto em base64|
 
 
-@app.route('/login')
+@app.route('/api_v1')
 def login():
-    return render_template('login.html')
-
-
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html')
-
-
-@app.route('/reconhecimento')
-def reconhecimento():
-    return render_template('reconhecimento.html')
-
-
-@app.route('/photo')
-def photo():
-    return render_template('photo.html')
-
-
-@app.route('/api', methods=['GET', 'POST'])
-def api():
-    if request.method == 'GET':
-        return jsonify({'msg': 'Hello World!'})
-    elif request.method == 'POST':
-        return jsonify({'msg': 'Hello World!'})
+    return 'login'
 
 
 if __name__ == '__main__':
